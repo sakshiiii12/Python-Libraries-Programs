c355eca2a3de74018fc4ed41badb399f767edc7b
sakshiiii12 <sakshiiii761@gmail.com>
2025-05-16 16:03:24 +0530

    printprogram

---
+++ Numpy Library Programs/1.createAnArray.py
@@ -0,0 +1,6 @@
+#Write a NumPy program to create an array of integers from 1 to 10.
+
+import numpy as np
+
+arr = np.array([[1,2,3,4,5], [6,7,8,9,10]])
+print("Array Of Integers From 1 to 10:","\n",arr)
