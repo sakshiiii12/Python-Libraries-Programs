ef64c0a2a8912a7ac44a7b8fcbfc2a0c11fc59e6
sakshiiii12 <sakshiiii761@gmail.com>
2025-05-16 18:54:16 +0530

    printprogram

---
+++ Numpy Library Programs/4.findShapeSizeOfArray.py
@@ -0,0 +1,8 @@
+#How do you find the shape and size of a numpy array.
+
+import numpy as np
+
+ar = np.array([[1,2,3], [4,5,6]])
+print("Array:","\n",ar)
+print("Shape:",ar.shape)
+print("Size:",ar.size)
