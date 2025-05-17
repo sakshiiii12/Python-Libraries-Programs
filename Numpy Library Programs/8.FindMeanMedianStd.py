0976c7b9799a01d174ff8f764a0a888826de1992
sakshiiii12 <sakshiiii761@gmail.com>
2025-05-17 16:50:54 +0530

    printprogram

---
+++ Numpy Library Programs/8.FindMeanMedianStd.py
@@ -0,0 +1,8 @@
+#Find a mean, median, and standard deviations in a Numpy array.
+
+import numpy as np
+
+arr = np.array([10,20,30,40,50])
+print("Mean of an array:", np.mean(arr))
+print("Median of an array:", np.median(arr))
+print("Standard Deviations of an array:", np.std(arr))
